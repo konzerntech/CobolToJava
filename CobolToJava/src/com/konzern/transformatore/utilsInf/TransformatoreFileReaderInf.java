8ae55aca92608107408616c1c2685e4ddeeb8663
KonzernTechie <kv.nijesh@gmail.com>
2019-04-06 11:15:36 +0530

    CreateInputFiles completed

---
+++ CobolToJava/src/com/konzern/transformatore/utilsInf/TransformatoreFileReaderInf.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.konzern.transformatore.utilsInf;
+
+/**
+ * @author Konzernite
+ * @since 2019-04-06
+ * @version 1.0
+ *
+ */
+public interface TransformatoreFileReaderInf {
+
+}
---
+++ CobolToJava/src/com/konzern/trasformatore/utils/TransformatoreFileReader.java
@@ -0,0 +1,80 @@
+/**
+ * TransformatoreFileReader.Java
+ * 
+ * <p>The aim of this class is to read all cobol file
+ * 
+ * Four main methods will be create, read, read&write, and close fileReader
+ * 
+ * BufferedReader to read line by line
+ * 
+ * location : E:\PROXIMOTECH\cobol_to_java_convertor\input_files </p>
+ */
+package com.konzern.trasformatore.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.konzern.transformatore.utilsInf.TransformatoreFileReaderInf;
+
+/**
+ * @author Konzerntech
+ * @since 2018-12-01
+ * @version 1.0
+ *
+ */
+public class TransformatoreFileReader implements TransformatoreFileReaderInf {
+
+	private static Map<String, BufferedReader> fileInputs = null;
+
+	static {
+		fileInputs = new HashMap<>();
+	}
+
+	/**
+	 * <p> To Read all cobol files in a particular directory
+	 * 
+	 * @param location
+	 * @param characterEncoding
+	 * @return Map<String, BufferedReader>
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static Map<String, BufferedReader> creatInputFiles(String location, String characterEncoding) throws FileNotFoundException, IOException {
+
+		File fileDirectory = new File(location);
+		for (File filePath : fileDirectory.listFiles()) {
+			BufferedReader br = null;
+			try (FileReader fr = new FileReader(filePath.getAbsolutePath())) {
+				br = new BufferedReader(fr);
+				fileInputs.put(filePath.getName(),br);
+				if(null!=br) {
+					br.close();
+				}
+			} 
+
+		}
+		return fileInputs;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Map<String, BufferedReader> i = creatInputFiles("E:\\PROXIMOTECH\\cobol_to_java_convertor\\input_files", "ASCII");
+			System.out.println(i);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
