f1d886ce5e57351793f1ebeea3727ece3cefdcf9
KonzernTechie <kv.nijesh@gmail.com>
2019-04-07 00:27:29 +0530

    Naming utility changed

--- CobolToJava/src/com/konzern/transformatore/java/JavaNamingUtiltiy.java
+++ CobolToJava/src/com/konzern/transformatore/java/JavaNamingUtiltiy.java
@@ -9,13 +9,14 @@ package com.konzern.transformatore.java;
 /**
  * @author konzernites
  * @version 1.0
- *
+ * @see https://www.oracle.com/technetwork/java/codeconventions-135099.html
  */
 public class JavaNamingUtiltiy {
 	
 	/**
 	 * 
 	 * To Create a valid java class Name
+	 * 
 	 * 
 	 * 
 	 * @param name
