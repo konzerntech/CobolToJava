26ad6ea9e6e3d29c6ef8eca6355763035be71840
KonzernTechie <kv.nijesh@gmail.com>
2019-04-07 12:08:41 +0530

    Pojo class for Java Code

---
+++ CobolToJava/src/com/konzern/transformatore/java/JavaPojo.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.konzern.transformatore.java;
+
+/**
+ * @author Apple
+ *
+ */
+public class JavaPojo {
+	
+	private String className = null;
+	private String authorName  = null;
+	private short version = 0;
+	
+	
+
+	public String getClassName() {
+		return className;
+	}
+
+
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
