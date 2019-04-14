f158d3f3908c5fe1a8d586d6a373bc30c9ae145f
KonzernTechie <kv.nijesh@gmail.com>
2019-04-15 02:18:55 +0530

    Cobol Division abstract

---
+++ CobolToJava/src/com/konzern/transformatore/cobol/CobolDivision.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.konzern.transformatore.cobol;
+
+import java.util.List;
+import java.util.Map;
+
+import com.konzern.transformatore.java.JavaPojo;
+
+/**
+ * @author Apple
+ *
+ */
+public abstract class CobolDivision extends Cobol {
+	
+	private IdentificationDivision identificationDivision = null; 
+	
+	@SuppressWarnings("unused")
+	private void codeConstructor(JavaPojo javaPojo, Map<String,List<String>> codeBlocks) {
+		
+		codeBlocks.forEach((key,elements)->{
+			
+			switch (key) {
+			case "IDENTIFICATION DIVISION.":
+				 identificationDivision = new IdentificationDivision(key, elements);
+				 identificationDivision.setJavaPojo(javaPojo);
+				 identificationDivision.getConvertedCode();
+				break;
+
+			default:
+				break;
+			}
+		});
+		
+	
+	}
+	
+
+
+}
