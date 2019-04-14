0fc84a4a452ce182732eca59312cd8f85ede79e1
KonzernTechie <kv.nijesh@gmail.com>
2019-04-15 01:47:09 +0530

    Division array list changed based on Enum

--- CobolToJava/src/com/konzern/transformatore/cobol/Cobol.java
+++ CobolToJava/src/com/konzern/transformatore/cobol/Cobol.java
@@ -2,11 +2,18 @@
  * Cobol.Java
  * 
  * An abstract class on the top of hierarchy
+ * 
  */
 package com.konzern.transformatore.cobol;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.konzern.transformatore.enums.CobolDivision;
 
 /**
  * @author Konzernites
@@ -17,12 +24,57 @@ public abstract class Cobol {
 	
 	public static  List<String> cobolDivisions = null;
 	
+	private Map<String,List<String>> codeBlocks = null;
+	
 	static {
 		cobolDivisions = new ArrayList<>();
+		
+		for(CobolDivision cobolDivison : CobolDivision.values()) {
+			cobolDivisions.add(cobolDivison.getDivision());
+		}
+		
+/*
+		
 		cobolDivisions.add("IDENTIFICATION DIVISION.");
 		cobolDivisions.add("DATA DIVISION.");
 		cobolDivisions.add("PROCEDURE DIVISION.");
-		cobolDivisions.add("ENVIRONMENT DIVISION.");
+		cobolDivisions.add("ENVIRONMENT DIVISION.");*/
 	}
+	
+	
+
+
+	/**
+	 * <p> split the COBOL code based on the division </p>
+	 * 
+	 * @param bufferedReader
+	 * @return
+	 * @throws IOException
+	 */
+	public Map<String,List<String>> divisionIdentifier(BufferedReader bufferedReader) throws IOException {
+		
+		if(null==bufferedReader) {
+			throw new IllegalArgumentException();
+		}
+		
+		codeBlocks  = new HashMap<>();
+		String line = null;
+		List<String> elements = null;
+		String key = null;
+		while (null != (line = bufferedReader.readLine())) {
+			if (CobolConstants.divisionIdentifier.contains(line)) {
+				if (null != elements) {
+					codeBlocks.put(key, elements);
+				}
+				key = line;
+				elements = new ArrayList<>();
+			} else {
+				elements.add(line);
+			}
+		}
+
+		return codeBlocks;
+	}
+	
 
 }
